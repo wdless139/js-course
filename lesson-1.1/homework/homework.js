aed6d80ce11eaf3dd2c7256f3224f1ae5f2d575e
Tanya Khonyakina <hanalinat@gmail.com>
2023-02-21 11:24:24 +0300

    Удалила лишние строки max и min, также циклы do...while. Добавила условную конструкцию в цикл for.

--- lesson-1.1/homework/homework.js
+++ lesson-1.1/homework/homework.js
@@ -63,22 +63,15 @@ findMaxNumber(setNum);
 // ****
 
 function outputStrings() {
-    let asterisk1 = '';
-    let asterisk2 = '';
-    let max = '*******';
-    let min = '****';
+    let asterisk1 = '*******';
+    let asterisk2 = '****';
 
-    do {
-        asterisk1 += '*';
-    } while (asterisk1.length < max.length);
-
-    do {
-        asterisk2 += '*';
-    } while (asterisk2.length < min.length);
-
-    for (let i = 0; i < 3; i++) {
-        console.log(asterisk1);
-        console.log(asterisk2);
+    for (let i = 0; i < 6; i++) {
+        if (i%2) {
+            console.log(asterisk2);
+        } else {
+            console.log(asterisk1);
+        }
     }
 }
 
