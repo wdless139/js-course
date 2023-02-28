3d10c5037cc1cbf80c34047e1d605b16ed7b309c
Tanya Khonyakina <hanalinat@gmail.com>
2023-02-28 14:54:25 +0300

    Исправила ошибки.

--- lesson-1.2/homework2/homework-2.js
+++ lesson-1.2/homework2/homework-2.js
@@ -1,18 +1,21 @@
 'use strict'
 
+const thisYear = 2023;
 const document = {
     number: 1234
 };
 
 function User(name, year, document) {
-    let thisYear = 2023;
-
     return {
         name: name,
         age: thisYear - year,
-        document: document?.number
+        document: document
     }
 }
 
 let user = new User('Denis', 2000, document);
-console.log(user);
+let user2 = new User('Tanus', 2002, { number: 4321 });
+let user3 = new User('HZ', 1999);
+console.log(user.document?.number);
+console.log(user2.document?.number);
+console.log(user3.document?.number);
