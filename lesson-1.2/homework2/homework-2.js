466f3cf855c937793d65cbf5af547d75bd132b0d
Tanya Khonyakina <hanalinat@gmail.com>
2023-02-28 15:07:00 +0300

    Переписала тело функции. Добавила this и удалила return

--- lesson-1.2/homework2/homework-2.js
+++ lesson-1.2/homework2/homework-2.js
@@ -6,16 +6,16 @@ const document = {
 };
 
 function User(name, year, document) {
-    return {
-        name: name,
-        age: thisYear - year,
-        document: document
+    {
+        this.name = name;
+        this.age = thisYear - year;
+        this.document = document;
     }
 }
 
 let user = new User('Denis', 2000, document);
 let user2 = new User('Tanus', 2002, { number: 4321 });
 let user3 = new User('HZ', 1999);
-console.log(user.document?.number);
+console.log(user);
 console.log(user2.document?.number);
 console.log(user3.document?.number);
