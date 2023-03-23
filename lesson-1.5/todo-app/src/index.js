4de8002260256c478df76ba218dabca67994cc3d
Денис Бондарчук <d.bondarchuk@worksolutions.ru>
2023-03-23 11:04:29 +0300

    Добавлена самостоятельная работа 2

---
+++ lesson-1.5/todo-app/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const db = require('./persistence');
+const getItems = require('./routes/getItems');
+const addItem = require('./routes/addItem');
+const updateItem = require('./routes/updateItem');
+const deleteItem = require('./routes/deleteItem');
+
+app.use(express.json());
+app.use(express.static(__dirname + '/static'));
+
+app.get('/items', getItems);
+app.post('/items', addItem);
+app.put('/items/:id', updateItem);
+app.delete('/items/:id', deleteItem);
+
+db.init().then(() => {
+    app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
